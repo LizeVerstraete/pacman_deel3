535c45b4e2f6c95d5462400230c07e7ef6332de1
Lize <Lize@192.168.0.187>
2021-05-15 12:17:14 +0200

    Testsuite verbeterd maar werkt nog niet

--- pacman_deel3/src/pacman/tests/WormholeTest.java
+++ pacman_deel3/src/pacman/tests/WormholeTest.java
@@ -1,25 +1,25 @@
 package pacman.tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Set;
 
+import org.junit.Test;
+
+import pacman.FoodItem;
 import pacman.Maze;
 import pacman.MazeDescriptions;
 import pacman.MazeMap;
 import pacman.PacMan;
+import pacman.PowerPellet;
 import pacman.Square;
 import pacman.wormholes.ArrivalPortal;
 import pacman.wormholes.DeparturePortal;
 import pacman.wormholes.Wormhole;
 
-public class WormholesTest {
+public class WormholeTest {
+
 	Maze maze = MazeDescriptions.createMazeFromDescription(new Random(), """
 			#####################
 			#.........#.........#
@@ -49,17 +49,21 @@ public class WormholesTest {
 			#...................#
 			#####################
 			""");
-	MazeMap mazeMap = maze.getMap();
+	
 	//DeparturePortal[] departurePortals = maze.getDeparturePortals();
 	//ArrivalPortal[] arrivalPortals = maze.getArrivalPortals();
 	//maze.addWormhole(new Wormhole(departurePortals[0], arrivalPortals[2]));
 	//maze.addWormhole(new Wormhole(departurePortals[0], arrivalPortals[0]));
 	//maze.addWormhole(new Wormhole(departurePortals[1], arrivalPortals[1]));
-	void test() {
+	MazeMap mazeMap = maze.getMap();
+	@Test
+	public void test() {
 		DeparturePortal departurePortal1 = new DeparturePortal(Square.of(mazeMap,1,1));
 		DeparturePortal	departurePortal2 = new DeparturePortal(Square.of(mazeMap, 5, 3));
-		ArrivalPortal arrivalPortal1 = new ArrivalPortal(Square.of(mazeMap,25,7));
+		ArrivalPortal arrivalPortal1 = new ArrivalPortal(Square.of(mazeMap,2,1));
 		ArrivalPortal arrivalPortal2 = new ArrivalPortal(Square.of(mazeMap, 1, 4));
+		//assertEquals(Set.of(departurePortal1,departurePortal2),maze.getDeparturePortals());
+		//assertEquals(Set.of(arrivalPortal1,arrivalPortal2), maze.getArrivalPortals());
 		Wormhole wormhole1 = new Wormhole(departurePortal1,arrivalPortal1);
 		Wormhole wormhole2 = new Wormhole(departurePortal2,arrivalPortal1);
 		maze.addWormhole(wormhole1);
@@ -67,7 +71,7 @@ public class WormholesTest {
 		
 		assertEquals(Square.of(mazeMap,1,1),departurePortal1.getSquare());
 		assertEquals(Square.of(mazeMap, 5, 3),departurePortal2.getSquare());
-		assertEquals(Square.of(mazeMap,25,7),arrivalPortal1.getSquare());
+		assertEquals(Square.of(mazeMap,2,1),arrivalPortal1.getSquare());
 		
 		assertEquals(arrivalPortal1,wormhole1.getArrivalPortal());
 		assertEquals(departurePortal2,wormhole2.getDeparturePortal());
@@ -85,7 +89,5 @@ public class WormholesTest {
 		assertEquals(departurePortal1,wormhole2.getDeparturePortal());
 		assertEquals(departurePortal2,wormhole1.getDeparturePortal());
 	}
+
 }
-
-
-
