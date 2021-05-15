b0d32a95679a64ad31e7b0a43f74e2df1c99273b
Lize <Lize@192.168.0.187>
2021-05-15 11:21:27 +0200

    WormholesTest gemaakt maar nog niet kunnen runnen

--- pacman_deel3/src/pacman/tests/WormholesTest.java
+++ pacman_deel3/src/pacman/tests/WormholesTest.java
@@ -1,7 +1,14 @@
 package pacman.tests;
 
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Set;
 
 import pacman.Maze;
 import pacman.MazeDescriptions;
@@ -39,16 +46,46 @@ public class WormholesTest {
 			###.#.#.#####.#.#.###
 			#.....#...#...#.....#
 			#.#######.#.#######.#
-			#A.................D#
+			#...................#
 			#####################
 			""");
 	MazeMap mazeMap = maze.getMap();
-	DeparturePortal[] departurePortals = maze.getDeparturePortals();
-	ArrivalPortal[] arrivalPortals = maze.getArrivalPortals();
+	//DeparturePortal[] departurePortals = maze.getDeparturePortals();
+	//ArrivalPortal[] arrivalPortals = maze.getArrivalPortals();
 	//maze.addWormhole(new Wormhole(departurePortals[0], arrivalPortals[2]));
 	//maze.addWormhole(new Wormhole(departurePortals[0], arrivalPortals[0]));
 	//maze.addWormhole(new Wormhole(departurePortals[1], arrivalPortals[1]));
-	DeparturePortal departurePortal1 = new DeparturePortal(Square.of(mazeMap,1,1));
-	DeparturePortal	departurePortal2 = new DeparturePortal(Square.of(mazeMap, 5, 3));
-	ArrivalPortal arrivalPortal1 = new ArrivalPortal(Square.of(mazeMap,25,7));
+	void test() {
+		DeparturePortal departurePortal1 = new DeparturePortal(Square.of(mazeMap,1,1));
+		DeparturePortal	departurePortal2 = new DeparturePortal(Square.of(mazeMap, 5, 3));
+		ArrivalPortal arrivalPortal1 = new ArrivalPortal(Square.of(mazeMap,25,7));
+		ArrivalPortal arrivalPortal2 = new ArrivalPortal(Square.of(mazeMap, 1, 4));
+		Wormhole wormhole1 = new Wormhole(departurePortal1,arrivalPortal1);
+		Wormhole wormhole2 = new Wormhole(departurePortal2,arrivalPortal1);
+		maze.addWormhole(wormhole1);
+		maze.addWormhole(wormhole2);
+		
+		assertEquals(Square.of(mazeMap,1,1),departurePortal1.getSquare());
+		assertEquals(Square.of(mazeMap, 5, 3),departurePortal2.getSquare());
+		assertEquals(Square.of(mazeMap,25,7),arrivalPortal1.getSquare());
+		
+		assertEquals(arrivalPortal1,wormhole1.getArrivalPortal());
+		assertEquals(departurePortal2,wormhole2.getDeparturePortal());
+		assertEquals(departurePortal1,wormhole1.getDeparturePortal());
+
+		assertEquals(Set.of(wormhole1,wormhole2),arrivalPortal1.getWormholes());
+		assertEquals(Set.of(wormhole1),departurePortal1.getWormholes());
+		assertEquals(Set.of(wormhole2),departurePortal2.getWormholes());
+		
+		wormhole1.setDeparturePortal(departurePortal2);
+		wormhole2.setDeparturePortal(departurePortal1);
+		wormhole1.setArrivalPortal(arrivalPortal2);
+		
+		assertEquals(arrivalPortal2,wormhole1.getArrivalPortal());
+		assertEquals(departurePortal1,wormhole2.getDeparturePortal());
+		assertEquals(departurePortal2,wormhole1.getDeparturePortal());
+	}
 }
+
+
+
